fe93fc1065e432eb67f7dc76f52273ad21acf889
Yonas Sisay <yonimelkamu357@gmail.com>
2023-05-13 23:33:53 +0300

    Hero section and the 3d computer object and the navigation component

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tilt from 'react-tilt'
+import {motion} from 'framer-motion'
+
+import {styles} from '../styles'
+import {services} from '../constants'
+import {fadeIn, textVariant} from '../utils/motion'
+const About = () => {
+  return (
+    ,
+  )
+}
+
+export default About
