5208cdbaddf7bfe0e1c130d39ed019fda47f9f8b
yonasisay <yonimelkamu357@gmail.com>
2024-03-04 10:44:51 +0300

    Add new images and icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
+/*-------------------images-------------------*/
 import logo from "./logo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
@@ -7,6 +8,7 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
+/*-------------------tech icons-------------------*/
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -22,58 +24,67 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
+/*-------------------company images-------------------*/
 import meta from "./company/meta.png";
 import getnet from "./company/getnet.png";
 import aastu from "./company/AASTU.jpg";
 import minabIcon from "./minabicon.png";
 
+/*-------------------project images-------------------*/
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 import shemach from "./shemach.png";
 import recipe from "./recipe.png";
 import recipe2 from "./recipe2.png";
+import kena from "./kena.png";
+import sheger from "./sheger.png";
+import plan from "./plan.png";
 
+/*-------------------social icons-------------------*/
 import instagram from "./instagram.png";
 import linkedin from "./linkedin.png";
 import telegram from "./telegram.png";
 import twitter from "./twitter.png";
 
 export {
-  logo,
-  backend,
-  creator,
-  mobile,
-  web,
-  github,
-  menu,
-  close,
-  css,
-  docker,
-  vue,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  getnet,
-  aastu,
-  carrent,
-  jobit,
-  tripguide,
-  instagram,
-  linkedin,
-  telegram,
-  twitter,
-  shemach,
-  recipe,
-  recipe2,
-  minabIcon,
+	logo,
+	backend,
+	creator,
+	mobile,
+	web,
+	github,
+	menu,
+	close,
+	css,
+	docker,
+	vue,
+	figma,
+	git,
+	html,
+	javascript,
+	mongodb,
+	nodejs,
+	reactjs,
+	redux,
+	tailwind,
+	typescript,
+	threejs,
+	meta,
+	getnet,
+	aastu,
+	carrent,
+	jobit,
+	tripguide,
+	instagram,
+	linkedin,
+	telegram,
+	twitter,
+	shemach,
+	recipe,
+	recipe2,
+	kena,
+	sheger,
+	plan,
+	minabIcon,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,269 +1,377 @@
 import {
-  mobile,
-  backend,
-  creator,
-  web,
-  javascript,
-  typescript,
-  html,
-  css,
-  reactjs,
-  vue,
-  redux,
-  tailwind,
-  nodejs,
-  mongodb,
-  git,
-  figma,
-  docker,
-  meta,
-  getnet,
-  aastu,
-  threejs,
-  github,
-  linkedin,
-  telegram,
-  twitter,
-  instagram,
-  shemach,
-  recipe,
-  recipe2,
-  minabIcon,
+	mobile,
+	backend,
+	creator,
+	web,
+	javascript,
+	typescript,
+	html,
+	css,
+	reactjs,
+	vue,
+	redux,
+	tailwind,
+	nodejs,
+	mongodb,
+	git,
+	figma,
+	docker,
+	meta,
+	getnet,
+	aastu,
+	threejs,
+	github,
+	linkedin,
+	telegram,
+	twitter,
+	instagram,
+	shemach,
+	recipe,
+	recipe2,
+	minabIcon,
+	kena,
+	sheger,
+	plan,
 } from "../assets";
 
 export const socialLinks = [
-  {
-    id: "github",
-    icon: github,
-    link: "https://github.com/yonaSisay/",
-    bg: "#181717",
-  },
-  {
-    id: "linkedin",
-    icon: linkedin,
-    link: "https://www.linkedin.com/in/yonas-sisay-62627822a/",
-    bg: "#0077B5",
-  },
-  {
-    id: "twitter",
-    icon: twitter,
-    link: "https://twitter.com/FikiruSisay",
-    bg: "#1DA1F2",
-  },
-  {
-    id: "instagram",
-    icon: instagram,
-    link: "https://www.instagram.com/j.w.yonas/",
-    bg: "#E1306C",
-  },
-  {
-    id: "telegram",
-    icon: telegram,
-    link: "https://telegram.com/jwyonas",
-    bg: "#0088CC",
-  },
+	{
+		id: "github",
+		icon: github,
+		link: "https://github.com/yonaSisay/",
+		bg: "#181717",
+	},
+	{
+		id: "linkedin",
+		icon: linkedin,
+		link: "https://www.linkedin.com/in/yonas-sisay-62627822a/",
+		bg: "#0077B5",
+	},
+	{
+		id: "twitter",
+		icon: twitter,
+		link: "https://twitter.com/FikiruSisay",
+		bg: "#1DA1F2",
+	},
+	{
+		id: "instagram",
+		icon: instagram,
+		link: "https://www.instagram.com/j.w.yonas/",
+		bg: "#E1306C",
+	},
+	{
+		id: "telegram",
+		icon: telegram,
+		link: "https://telegram.com/jwyonas",
+		bg: "#0088CC",
+	},
 ];
 
 export const navLinks = [
-  {
-    id: "about",
-    title: "About",
-  },
-  {
-    id: "work",
-    title: "Work",
-  },
-  {
-    id: "contact",
-    title: "Contact",
-  },
+	{
+		id: "about",
+		title: "About",
+	},
+	{
+		id: "work",
+		title: "Work",
+	},
+	{
+		id: "contact",
+		title: "Contact",
+	},
 ];
 
 const services = [
-  {
-    title: "Web Developer",
-    icon: web,
-  },
-  {
-    title: "React  Developer",
-    icon: web,
-  },
-  {
-    title: "Backend Developer",
-    icon: backend,
-  },
-  {
-    title: "Vue  Developer",
-    icon: web,
-  },
+	{
+		title: "Web Developer",
+		icon: web,
+	},
+	{
+		title: "React  Developer",
+		icon: web,
+	},
+	{
+		title: "Backend Developer",
+		icon: backend,
+	},
+	{
+		title: "Vue  Developer",
+		icon: web,
+	},
 ];
 
 const technologies = [
-  {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
-  {
-    name: "JavaScript",
-    icon: javascript,
-  },
-  {
-    name: "TypeScript",
-    icon: typescript,
-  },
-  {
-    name: "React JS",
-    icon: reactjs,
-  },
-  {
-    name: "Vue js",
-    icon: vue,
-  },
-  {
-    name: "Redux Toolkit",
-    icon: redux,
-  },
-  {
-    name: "Tailwind CSS",
-    icon: tailwind,
-  },
-  {
-    name: "Node JS",
-    icon: nodejs,
-  },
-  {
-    name: "MongoDB",
-    icon: mongodb,
-  },
-  {
-    name: "Three JS",
-    icon: threejs,
-  },
-  {
-    name: "git",
-    icon: git,
-  },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "docker",
-    icon: docker,
-  },
+	{
+		name: "HTML 5",
+		icon: html,
+	},
+	{
+		name: "CSS 3",
+		icon: css,
+	},
+	{
+		name: "JavaScript",
+		icon: javascript,
+	},
+	{
+		name: "TypeScript",
+		icon: typescript,
+	},
+	{
+		name: "React JS",
+		icon: reactjs,
+	},
+	{
+		name: "Vue js",
+		icon: vue,
+	},
+	{
+		name: "Redux Toolkit",
+		icon: redux,
+	},
+	{
+		name: "Tailwind CSS",
+		icon: tailwind,
+	},
+	{
+		name: "Node JS",
+		icon: nodejs,
+	},
+	{
+		name: "MongoDB",
+		icon: mongodb,
+	},
+	{
+		name: "Three JS",
+		icon: threejs,
+	},
+	{
+		name: "git",
+		icon: git,
+	},
+	{
+		name: "figma",
+		icon: figma,
+	},
+	{
+		name: "docker",
+		icon: docker,
+	},
 ];
 
 const experiences = [
-  {
-    title: "Software Engineering student ",
-    company_name: "AASTU",
-    icon: aastu,
-    website: "http://www.aastu.edu.et/",
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Studied various programming languages and technology stacks.",
-      "Participated in a variety of projects, both collaboratively within a team and individually.",
-      "Acquired knowledge in diverse Software Development Life Cycle (SDLC) methodologies and gained  documenting project requirements.",
-      "Developed teamwork skills and gained practical experience in collaborating effectively within a group.",
-    ],
-  },
+	{
+		title: "Software Engineering student ",
+		company_name: "AASTU",
+		icon: aastu,
+		website: "http://www.aastu.edu.et/",
+		iconBg: "#E6DEDD",
+		date: "Jan 2023 - Present",
+		points: [
+			"Studied various programming languages and technology stacks.",
+			"Participated in a variety of projects, both collaboratively within a team and individually.",
+			"Acquired knowledge in diverse Software Development Life Cycle (SDLC) methodologies and gained  documenting project requirements.",
+			"Developed teamwork skills and gained practical experience in collaborating effectively within a group.",
+		],
+	},
 
-  {
-    title: "Front end Developer intern",
-    company_name: "Get-net software solutions",
-    icon: getnet,
-    website: "https://www.getnetsoft.com/",
-    iconBg: "#E6DEDD",
-    date: "Jun 2023 - sept 2023",
-    points: [
-      "Developing  web application using React.js and other related technologies.",
-      "Collaborating with cross-functional teams ",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Web developer",
-    company_name: "Minab it solutions",
-    website: "https://www.minabtech.com/",
-    icon: minabIcon,
-    iconBg: "#E6DEDD",
-    date: "Oct 2023 - present",
-    points: [
-      "Developing  web application using Vue.js Go and Hasura.",
-      "Collaborating with cross-functional teams ",
-      "Implementing responsive design and ensuring cross-browser compatibility using tailwind.",
-      "Creating enterprise level website with Hasura and Nuxt.js",
-    ],
-  },
+	{
+		title: "Front end Developer intern",
+		company_name: "Get-net software solutions",
+		icon: getnet,
+		website: "https://www.getnetsoft.com/",
+		iconBg: "#E6DEDD",
+		date: "Jun 2023 - sept 2023",
+		points: [
+			"Developing  web application using React.js and other related technologies.",
+			"Collaborating with cross-functional teams ",
+			"Implementing responsive design and ensuring cross-browser compatibility.",
+			"Participating in code reviews and providing constructive feedback to other developers.",
+		],
+	},
+	{
+		title: "Web developer",
+		company_name: "Minab it solutions",
+		website: "https://www.minabtech.com/",
+		icon: minabIcon,
+		iconBg: "#E6DEDD",
+		date: "Oct 2023 - present",
+		points: [
+			"Developing  web application using Vue.js Go and Hasura.",
+			"Collaborating with cross-functional teams ",
+			"Implementing responsive design and ensuring cross-browser compatibility using tailwind.",
+			"Creating enterprise level website with Hasura and Nuxt.js",
+		],
+	},
 ];
 
 const projects = [
-  {
-    name: "Delicious recipe",
-    description:
-      "Indulge in culinary exploration! Discover and share delectable recipes on this intuitive, user-friendly platform designed for passionate food enthusiasts. Join the flavorful journey!",
-    tags: [
-      {
-        name: "Vue",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "Hasura",
-        color: "green-text-gradient",
-      },
-      {
-        name: "graphql",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "Go",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "Pinia",
-        color: "blue-text-gradient",
-      },
-    ],
-    image: recipe,
-    source_code_link: "https://github.com/yonaSisay/recipe_frontend",
-    web_link: "https://recipe-frontend-omega.vercel.app/",
-  },
-  {
-    name: "Shemach",
-    description:
-      "simple ecommerce platform that help users to make order of product and see the product they orderd, help them to search product and checkout",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "firebase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "Redux",
-        color: "blue-text-gradient",
-      },
-    ],
-    image: shemach,
-    source_code_link: "https://github.com/yonaSisay/shemach",
-    web_link: "https://shemach-612da.web.app/",
-  },
+	{
+		name: "Kena",
+		description:
+			"Kena, under EYEA, fuels youth entrepreneurship through training, mentorship, and financial support. Its innovation competitions and strategic partnerships enrich a dynamic ecosystem, propelling the success of youth-led businesses.",
+		tags: [
+			{
+				name: "Vue",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Nuxt.js",
+				color: "green-text-gradient",
+			},
+			{
+				name: "Vue Apollo",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "graphql",
+				color: "pink-text-gradient",
+			},
+
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Pinia",
+				color: "blue-text-gradient",
+			},
+		],
+		image: kena,
+		source_code_link: "",
+		web_link: "https://kena.eyea.et/",
+	},
+	{
+		name: "Sheger Gebeta",
+		description:
+			"Indulge in culinary exploration! Discover and share delectable recipes on this intuitive, user-friendly platform designed for passionate food enthusiasts. Join the flavorful journey!",
+		tags: [
+			{
+				name: "Vue",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Nuxt.js",
+				color: "green-text-gradient",
+			},
+			{
+				name: "Vue Apollo",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "graphql",
+				color: "pink-text-gradient",
+			},
+
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Pinia",
+				color: "blue-text-gradient",
+			},
+		],
+		image: sheger,
+		source_code_link: "",
+		web_link: "https://preview.shegergebeta.com/",
+	},
+	{
+		name: "Plan international",
+		description:
+			"The goal of the project is to empower working and stay-at-home moms through establishing home-based childcare services in their own homes. In doing so, they will have their own business in a sector that is in high demand and continues to do so.",
+		tags: [
+			{
+				name: "Vue",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Nuxt.js",
+				color: "green-text-gradient",
+			},
+			{
+				name: "Vue Apollo",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "graphql",
+				color: "pink-text-gradient",
+			},
+
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Pinia",
+				color: "blue-text-gradient",
+			},
+		],
+		image: plan,
+		source_code_link: "",
+		web_link: "https://staging.uymp.org/",
+	},
+	{
+		name: "Delicious recipe",
+		description:
+			"Indulge in culinary exploration! Discover and share delectable recipes on this intuitive, user-friendly platform designed for passionate food enthusiasts. Join the flavorful journey!",
+		tags: [
+			{
+				name: "Vue",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Hasura",
+				color: "green-text-gradient",
+			},
+			{
+				name: "graphql",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Go",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Pinia",
+				color: "blue-text-gradient",
+			},
+		],
+		image: recipe,
+		source_code_link: "https://github.com/yonaSisay/recipe_frontend",
+		web_link: "https://recipe-frontend-omega.vercel.app/",
+	},
+	{
+		name: "Shemach",
+		description:
+			"simple ecommerce platform that help users to make order of product and see the product they orderd, help them to search product and checkout",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "firebase",
+				color: "green-text-gradient",
+			},
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Redux",
+				color: "blue-text-gradient",
+			},
+		],
+		image: shemach,
+		source_code_link: "https://github.com/yonaSisay/shemach",
+		web_link: "https://shemach-612da.web.app/",
+	},
 ];
 
 export { services, technologies, experiences, projects };
